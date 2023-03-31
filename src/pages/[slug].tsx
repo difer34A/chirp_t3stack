250d5e223fe2d8caa2c291caa16d73d82d13211a
Dfr <difer34A@gmail.com>
2023-03-31 22:03:47 +0800

    minor fixes

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -40,15 +40,14 @@ const ProfilePage: NextPage<{ username: string }> = ({ username }) => {
             <div className="relative h-36 bg-slate-600">
             <Image
                 src={data.profileImageUrl}
-                alt={`${data.username ?? data.externalUsername}'s profile pic`}
+                alt={`profile pic`}
                 width={128}
                 height={128}
                 className="absolute bottom-0 left-0 -mb-[64px] ml-4 rounded-full border-4 border-black bg-black"
             />
             </div>
             <div className="h-[64px]"></div>
-            <div className="p-4 text-2xl font-bold">{`@${data.username ?? data.externalUsername
-            }`}</div>
+            <div className="p-4 text-2xl font-bold">@{data.username}</div>
             <div className="w-full border-b border-slate-400" />
         </div>
         
